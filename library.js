91ce21684cac78a83b96e382bdf9d5988e4f1e64
TedVJ <tedr56@gmail.com>
2014-11-13 20:25:47 +0100

    Fix library.js

--- library.js
+++ library.js
@@ -4,7 +4,7 @@
 	var Vimeo = {},
 		embed = '<iframe class="vimeo-embed" src="//player.vimeo.com/video/$1" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 
-    embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)">.+<\/a>/g;
+    var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)">.+<\/a>/g;
 
 	Vimeo.parse = function(data, callback) {
 	    if (!data || !data.postData || !data.postData.content) {
