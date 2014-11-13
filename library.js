3df0cbb13e307eae6a710f848c99e4145b16e246
TedVJ <tedr56@gmail.com>
2014-11-13 20:22:16 +0100

    Update to 0.2.0 - NodeBB 0.6.0 Compatibility

--- library.js
+++ library.js
@@ -4,13 +4,17 @@
 	var Vimeo = {},
 		embed = '<iframe class="vimeo-embed" src="//player.vimeo.com/video/$1" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 
+    embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)">.+<\/a>/g;
 
-
-	Vimeo.parse = function(postContent, callback) {
-		// modified from http://stackoverflow.com/questions/7168987/
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)">.+<\/a>/g, embed);
-		callback(null, postContent);
-	};
+	Vimeo.parse = function(data, callback) {
+	    if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(embedUrl)) {
+            data.postData.content = data.postData.content.replace(embedUrl, embed);
+        }
+        callback(null, data);
+    };
 
 	module.exports = Vimeo;
 }(module));
